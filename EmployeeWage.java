f89f7d86b7e72f32eb7ceb560c0392392258b97f
harshrastogi15 <hrastogi2002@gmail.com>
2023-11-22 19:29:25 +0530

    Refactored: changed if else to switch

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,26 +3,42 @@ class EmployeeWage {
     public static final int IS_PART_TIME = 2;
     public static final int EMP_WAGE_PER_HOUR = 20;
 
-    public static int employeeCheck(){
-        return (int)(Math.ceil(Math.random()*10)%3);
+    public static int employeeCheck() {
+        return (int) (Math.ceil(Math.random() * 10) % 3);
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
         int employeePresent = employeeCheck();
         int empHours;
-        if(employeePresent == IS_FULL_TIME){
-            System.out.println("Employee is Full Time");
-            empHours = 8;
-        }else if(employeePresent == IS_PART_TIME){
-            System.out.println("Employee is Part Time");
-            empHours = 4;
+        // if(employeePresent == IS_FULL_TIME){
+        // System.out.println("Employee is Full Time");
+        // empHours = 8;
+        // }else if(employeePresent == IS_PART_TIME){
+        // System.out.println("Employee is Part Time");
+        // empHours = 4;
+        // }
+        // else{
+        // System.out.println("Employee is absent");
+        // empHours = 0;
+        // }
+
+        switch (employeePresent) {
+            case IS_FULL_TIME:
+                System.out.println("Employee is Full Time");
+                empHours = 8;
+                break;
+            case IS_PART_TIME:
+                System.out.println("Employee is Part Time");
+                empHours = 4;
+                break;
+            default:
+                System.out.println("Employee is absent");
+                empHours = 0;
+                break;
         }
-        else{
-            System.out.println("Employee is absent");
-            empHours = 0;
-        }
-        int dailyWage = empHours*EMP_WAGE_PER_HOUR;
+
+        int dailyWage = empHours * EMP_WAGE_PER_HOUR;
         System.out.println("Daily Wage: " + dailyWage);
     }
 }
