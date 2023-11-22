a6da7c1d473913cbcc116b5c71490e83e7b0e0fb
harshrastogi15 <hrastogi2002@gmail.com>
2023-11-22 19:34:41 +0530

    Refactored: Calculating Wages for a month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,6 +2,7 @@ class EmployeeWage {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
     public static final int EMP_WAGE_PER_HOUR = 20;
+    public static final int MONTHLY_WORKING_DAYS = 30;
 
     public static int employeeCheck() {
         return (int) (Math.ceil(Math.random() * 10) % 3);
@@ -9,33 +10,24 @@ class EmployeeWage {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
-        int employeePresent = employeeCheck();
-        int empHours;
-        // if(employeePresent == IS_FULL_TIME){
-        // System.out.println("Employee is Full Time");
-        // empHours = 8;
-        // }else if(employeePresent == IS_PART_TIME){
-        // System.out.println("Employee is Part Time");
-        // empHours = 4;
-        // }
-        // else{
-        // System.out.println("Employee is absent");
-        // empHours = 0;
-        // }
-
-        switch (employeePresent) {
-            case IS_FULL_TIME:
+        int empHours=0;
+        // UC5 - Calculate Wages for month
+        for(int i=1;i<=MONTHLY_WORKING_DAYS;i++){
+            int employeePresent = employeeCheck();
+            switch (employeePresent) {
+                case IS_FULL_TIME:
                 System.out.println("Employee is Full Time");
-                empHours = 8;
+                empHours += 8;
                 break;
-            case IS_PART_TIME:
+                case IS_PART_TIME:
                 System.out.println("Employee is Part Time");
-                empHours = 4;
+                empHours += 4;
                 break;
-            default:
+                default:
                 System.out.println("Employee is absent");
-                empHours = 0;
+                empHours += 0;
                 break;
+            }
         }
 
         int dailyWage = empHours * EMP_WAGE_PER_HOUR;
