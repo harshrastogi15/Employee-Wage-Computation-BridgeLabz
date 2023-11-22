8cdc75a50b6d80c40f6c430209b356cac6b8684d
harshrastogi15 <hrastogi2002@gmail.com>
2023-11-22 19:41:17 +0530

    Refactored: Calculating Wages until condition false

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,6 +3,7 @@ class EmployeeWage {
     public static final int IS_PART_TIME = 2;
     public static final int EMP_WAGE_PER_HOUR = 20;
     public static final int MONTHLY_WORKING_DAYS = 30;
+    public static final int MAX_HOURS_IN_MONTH = 50;
 
     public static int employeeCheck() {
         return (int) (Math.ceil(Math.random() * 10) % 3);
@@ -10,9 +11,9 @@ class EmployeeWage {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
-        int empHours=0;
-        // UC5 - Calculate Wages for month
-        for(int i=1;i<=MONTHLY_WORKING_DAYS;i++){
+        int empHours=0, totalDays = 0;
+        // UC6 - Calculate Wages till a condition of total working hours or days is reached for a month
+        while(empHours<MAX_HOURS_IN_MONTH && totalDays<MONTHLY_WORKING_DAYS){
             int employeePresent = employeeCheck();
             switch (employeePresent) {
                 case IS_FULL_TIME:
