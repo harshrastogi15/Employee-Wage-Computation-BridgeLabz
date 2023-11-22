eeabd679c7e2ab6a86dd86924072404aea35fcce
harshrastogi15 <hrastogi2002@gmail.com>
2023-11-22 19:09:44 +0530

    Refactored: Added new method employee check

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,5 +1,20 @@
 class EmployeeWage {
+    public static final int IS_FULL_TIME = 1;
+
+    // UC1 - Employee is Present or Absent
+    public static int employeeCheck(){
+        return (int)(Math.ceil(Math.random()*10)%2);
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+
+        int employeePresent = employeeCheck();
+        if(employeePresent == IS_FULL_TIME){
+            System.out.println("Employee is present");
+        }else{
+            System.out.println("Employee is absent");
+        }
+
     }
 }
