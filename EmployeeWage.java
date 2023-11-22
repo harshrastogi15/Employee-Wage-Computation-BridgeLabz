fb90e0fd8583f11f120cc792ab159c86e1564ed0
harshrastogi15 <hrastogi2002@gmail.com>
2023-11-22 19:24:36 +0530

    Refactored: Added Part Time Employee

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,10 +1,10 @@
 class EmployeeWage {
     public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
     public static final int EMP_WAGE_PER_HOUR = 20;
 
-    // UC1 - Employee is Present or Absent
     public static int employeeCheck(){
-        return (int)(Math.ceil(Math.random()*10)%2);
+        return (int)(Math.ceil(Math.random()*10)%3);
     }
 
     public static void main(String[] args) {
@@ -12,13 +12,16 @@ class EmployeeWage {
         int employeePresent = employeeCheck();
         int empHours;
         if(employeePresent == IS_FULL_TIME){
-            System.out.println("Employee is present");
+            System.out.println("Employee is Full Time");
             empHours = 8;
-        }else{
+        }else if(employeePresent == IS_PART_TIME){
+            System.out.println("Employee is Part Time");
+            empHours = 4;
+        }
+        else{
             System.out.println("Employee is absent");
             empHours = 0;
         }
-        // UC2 - Calculate Daily Employee Wage
         int dailyWage = empHours*EMP_WAGE_PER_HOUR;
         System.out.println("Daily Wage: " + dailyWage);
     }
